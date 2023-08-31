ccd06a2d3f2576a49fc9f488fcb415c8ca86cf06
João Cassanji <joaocassanji@gmail.com>
2023-08-31 13:34:18 -0300

    added module 8

---
+++ go-fundamentals-course-PS/functions/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type menuItem struct {
+	name   string
+	prices map[string]float64
+}
+
+var menu = []menuItem{
+	{name: "Coffee", prices: map[string]float64{"Small": 1.65, "Medium": 1.80, "Large": 1.95}},
+	{name: "Espresso", prices: map[string]float64{"Single": 2.65, "Double": 2.80, "Triple": 2.95}},
+}
+
+var in = bufio.NewReader(os.Stdin)
+
+func main() {
+loop:
+	for {
+
+		fmt.Println("Please select a option")
+		fmt.Println("1) Print menu")
+		fmt.Println("2) Add item")
+		fmt.Println("q) quit")
+
+		choice, _ := in.ReadString('\n')
+		choice = strings.TrimSpace(choice)
+
+		switch choice {
+		case "1":
+			printMenu()
+		case "2":
+			addItem()
+		case "q":
+			break loop
+		default:
+			fmt.Println("Unknown option")
+		}
+	}
+}
+
+func printMenu() {
+	for _, item := range menu {
+		fmt.Println(item.name)
+		fmt.Println(strings.Repeat("-", 10))
+		for size, price := range item.prices {
+			fmt.Printf("\t%10s%10.2f\n", size, price)
+		}
+	}
+}
+
+func addItem() {
+	fmt.Println("Please enter the name of the new item")
+	name, _ := in.ReadString('\n')
+	menu = append(menu, menuItem{name: strings.TrimSpace(name), prices: map[string]float64{"Single": 2.65, "Double": 2.80, "Triple": 2.95}})
+}
