ba90ca307ac0695482e31dca914a9daf9d6c6979
João Cassanji <joaocassanji@gmail.com>
2023-09-04 00:30:32 -0300

    added final modules

---
+++ acessing-relational-databases-in-go-course-PS/stored-procedures/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+	dsn := mysql.Config{
+		User:                 "root",
+		Passwd:               "root",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "sakila",
+		AllowNativePasswords: true,
+	}
+
+	var err error
+	db, err = sql.Open("mysql", dsn.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+
+	actors, err := GetActorSP("RIVER", "DEAN")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Actor found: %v\n", actors)
+}
+
+type Actor struct {
+	actor_id   int64
+	first_name string
+	last_name  string
+}
+
+func GetActorSP(first_name string, last_name string) ([]Actor, error) {
+	var actors []Actor
+
+	result, err := db.Query("CALL addActor (?, ?)", first_name, last_name)
+	if err != nil {
+		return nil, fmt.Errorf("GetActorSP: %v", err)
+	}
+	defer result.Close()
+
+	// Loop through rows
+	for result.Next() {
+		var acts Actor
+		if err := result.Scan(&acts.actor_id, &acts.first_name, &acts.last_name); err != nil {
+			return nil, fmt.Errorf("GetActorSP: %v", err)
+		}
+		actors = append(actors, acts)
+
+		if err := result.Err(); err != nil {
+			return nil, fmt.Errorf("GetActorSP: %v", err)
+		}
+	}
+	return actors, nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module demo
 go 1.21.0
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
 	github.com/golang-sql/sqlexp v0.1.0 // indirect
